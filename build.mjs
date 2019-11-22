6da4e0c5c9f9e0eda5b8c4a48a1c6a596a10b9ea
Captain Fantasy <captain@fantasyui.com>
2019-11-21 19:50:09 -0500

    System Upgrade, custom build system.

--- build.mjs
+++ build.mjs
@@ -2,6 +2,7 @@
 import fs from 'fs-extra';
 import path from 'path';
 
+import request from 'request';
 import cheerio from 'cheerio';
 import lodash from 'lodash';
 import chalk from 'chalk';
@@ -32,27 +33,33 @@ async function copyAssets(configuration){
 
 async function renderFiles(configuration){
   console.log(chalk.yellow('Rendering Files'))
+
   for(const item of configuration.chapters){
       console.log(`Rendering ${item.name}`);
       const filename = item.name + '.html';
       const original = (await fs.readFile(path.join(configuration.source, filename))).toString()
-      const rendered = await renderFile(original);
+
+      // let rendered = await patchOldStuff(original);
+      let rendered = await renderComponents(original, configuration);
+
       //console.log(rendered)
-      //await fs.writeFile(path.join(configuration.destination, filename))
+      await fs.writeFile(path.join(configuration.destination, filename), rendered)
   }
+
 }
 
-async function renderFile(html){
-  const $ = cheerio.load(html,{xml:{normalizeWhitespace: true}});
+async function patchOldStuff(html){
+  const $ = cheerio.load(html);
 
   $('p > a > img').each(function(i, elem) {
 
     const imageHref = $(this).attr('src');
+    const imageAlt = $(this).attr('alt');
     let youtubeId = null;
     const match = imageHref.match(/http:\/\/img\.youtube\.com\/vi\/([a-zA-Z0-9_-]+)\/0.jpg$/)
     if(match){
       youtubeId = match[1];
-      $(this).parent().parent().html(`<div class="widget youtube" data-id="${youtubeId}"></div>`)
+      $(this).parent().parent().html(`<div class="widget youtube" data-id="${youtubeId}" data-title="${imageAlt}"></div>`)
     }else{
       console.log('Unable to extract id from ', imageHref);
     }
@@ -61,4 +68,69 @@ async function renderFile(html){
   });
 
   return $.root().html();
+}
+
+async function renderComponents(html, configuration){
+  const $ = cheerio.load(html);
+  const thumbnailDownloads = [];
+
+  $('div.widget.youtube').each(function(i, elem) {
+
+    const youtubeId = $(this).data('id');
+    const youtubeTitle = $(this).data('title');
+
+    // donload thumbnail image
+    const thumbUrl = `http://img.youtube.com/vi/${youtubeId}/0.jpg`;
+    const saveThumbAs = path.resolve(`${configuration.source}/images/youtube-${youtubeId}.jpg`);
+    thumbnailDownloads.push([thumbUrl,saveThumbAs]);
+
+
+
+    const widgetHtml = `
+      <div class="card text-white bg-dark">
+        <div class="card-header">
+          ${youtubeTitle}
+        </div>
+        <div class="card-video">
+          <a href="https://www.youtube.com/watch?v=${youtubeId}"><img class="card-img-bottom" src="images/youtube-${youtubeId}.jpg" alt="${youtubeTitle}"></a>
+          <img class="video-play" src="images/video-play.png">
+        </div>
+
+
+      </div>
+    `;
+    $(this).html(widgetHtml);
+
+
+  });
+
+
+  for(const [thumbUrl, saveThumbAs] of thumbnailDownloads){
+    await downloadYoutubeImage(thumbUrl, saveThumbAs);
+  }
+
+  return $.root().html();
+}
+
+
+function downloadYoutubeImage(src, dest){
+  return new Promise(async function(resolve, reject) {
+
+    if(await fs.pathExists(dest)) {
+      //console.log('already downloaded, exit early',dest);
+      resolve();
+      return;
+    }else{
+      //console.log('Downloading %s into %s', src, dest);
+    }
+
+    request(src).pipe(fs.createWriteStream(dest)).on('close', function(err){
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve()
+    });
+
+  });
 }
